8d92a43a2523062d4f7ab8f78977aa53d64bb10c
JAlbertoCastaneda <A23310352@live.ceti.mx>
2024-12-09 12:25:50 -0600

    c4

--- src/Shoot.cpp
+++ src/Shoot.cpp
@@ -1,20 +1,41 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <cmath>
+
+constexpr float M_P = 3.14159265f;
+
+ constexpr float TURN_SPEED = 200.0f;
+ constexpr float PLAYER_SPEED = 200.0f;
 
 class Player {
 public:
     Player()
-        : position(500, 500), angle(45), array(sf::Quads, 4) {
+        : position(500, 500), angle(0), array(sf::Quads, 4) {
         // Coordenadas relativas al centro del jugador
-        array[0].position = sf::Vector2f(0, -30);
-        array[1].position = sf::Vector2f(-30, 30);
-        array[2].position = sf::Vector2f(0, 15);
-        array[3].position = sf::Vector2f(30, 30);
+        array[0].position = sf::Vector2f(20, 0);
+        array[1].position = sf::Vector2f(-30, -20);
+        array[2].position = sf::Vector2f(-15, 0);
+        array[3].position = sf::Vector2f(-30, 20);
 
         // Asignar colores para cada vértice (opcional)
         for (size_t i = 0; i < array.getVertexCount(); i++) {
             array[i].color = sf::Color::White;
         }
+    }
+    void update(float deltaTime){
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            angle -= TURN_SPEED * deltaTime;
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            angle += TURN_SPEED * deltaTime;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
+            float radians = angle * (M_P/180.0f);
+            position.x += cos(radians) * PLAYER_SPEED * deltaTime;
+            position.y += sin(radians) * PLAYER_SPEED * deltaTime;
+
+        }
+            
     }
 
     void Draw(sf::RenderWindow& window) {
@@ -48,8 +69,8 @@ int main() {
             }
         }
 
+        player.update(deltaTime);
         window.clear(sf::Color::Black); // Limpiar la ventana con color negro
-
         // Dibujar el jugador
         player.Draw(window);
 
