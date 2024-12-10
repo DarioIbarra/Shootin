2b598cf7a3505a7fd92c29ee494e28b2a719d1a3
JAlbertoCastaneda <A23310352@live.ceti.mx>
2024-12-09 19:40:44 -0600

    c6

--- src/Shoot.cpp
+++ src/Shoot.cpp
@@ -2,12 +2,16 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <list>
 
 constexpr float M_P = 3.14159265f;
 
  constexpr float TURN_SPEED = 200.0f;
  constexpr float PLAYER_SPEED = 200.0f;
+ constexpr float SHOOT_DELAY = 0.2f;
  constexpr float BULLET_SPEED = 400.0f;
+ constexpr float BULLET_LIFE = 3.0f;
+
 class Entity {
     public:
         Entity(sf::Vector2f position, float angle)
@@ -19,14 +23,21 @@ class Entity {
 };
 
 std::vector<Entity*> entities{};
+std::list<std::vector<Entity*>::iterator> toRemoveList{};
+std::list<Entity*> toAddList{};
 
 class Bullet : public Entity {
     public:
        Bullet(sf::Vector2f position, sf::Vector2f direction)
-            : shape(1.0f), direction(direction), Entity(position, 0.0f){}
+            : shape(1.0f), direction(direction), Entity(position, 0.0f), lifetime(BULLET_LIFE){}
 
         void update(float deltaTime) override {
+            lifetime -= deltaTime;
             position += direction * BULLET_SPEED * deltaTime;
+            if(lifetime <= 0.0f){
+                toRemoveList.push_back(std::find(entities.begin(), entities.end(), this));
+                delete this;
+            }
         }
         void render(sf::RenderWindow& window) override {
             window.draw(shape, sf::Transform().translate(position));
@@ -35,12 +46,13 @@ class Bullet : public Entity {
     private:
         sf::CircleShape shape;
         sf::Vector2f direction;
+        float lifetime;
 };
 
 class Player : public Entity {
 public:
     Player()
-        : Entity(sf::Vector2f(500, 500),0), array(sf::Quads, 4) {
+        : Entity(sf::Vector2f(500, 500),0), array(sf::Quads, 4), shootTimer() {
         // Coordenadas relativas al centro del jugador
         array[0].position = sf::Vector2f(20, 0);
         array[1].position = sf::Vector2f(-30, -20);
@@ -53,6 +65,7 @@ public:
         }
     }
     void update(float deltaTime) override {
+        shootTimer -= deltaTime;
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
             angle -= TURN_SPEED * deltaTime;
         }
@@ -66,9 +79,10 @@ public:
 
         }
 
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && shootTimer <= 0.0f) {
+            shootTimer = SHOOT_DELAY;
             float radians = angle * (M_P/180.0f);
-            entities.push_back(new Bullet(position, sf::Vector2f(cos(radians),sin(radians))));
+            toAddList.push_back(new Bullet(position, sf::Vector2f(cos(radians),sin(radians))));
         }
             
     }
@@ -80,6 +94,7 @@ public:
 
 private:
     sf::VertexArray array;
+    float shootTimer;
 };
 
 int main() {
@@ -95,15 +110,28 @@ int main() {
         while (window.pollEvent(e)) {
             if (e.type == sf::Event::Closed) {
                 window.close();
+            } else if (e.type = sf::Event::KeyPressed){
+                if(e.key.code = sf::Keyboard::Q){
+                    printf("%d\n", entities.size());
+                }
             }
         }
-
+        toAddList.clear();
+        toRemoveList.clear();
         window.clear(sf::Color::Black); // Limpiar la ventana con color negro
 
         for(size_t i =0; i<entities.size(); i++){
             entities[i]->update(deltaTime);
             entities[i]->render(window);
         }
+        for (const auto& it : toRemoveList){
+            delete* it;
+            entities.erase(it);
+        }
+        for (auto& ptr : toAddList){
+            entities.push_back(ptr);
+        }
+        
        
         window.display(); // Mostrar todo lo dibujado
     }
